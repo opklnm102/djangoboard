400efa71226853be9b919eac438b8de42701b799
DongHee Kim <opklnm102@gmail.com>
2017-03-13 21:12:40 +0900

    Form에 예외 처리 추가

--- user_manager/forms.py
+++ user_manager/forms.py
@@ -11,4 +11,6 @@ class JoinForm(forms.Form):
     id = forms.CharField(label="ID", min_length=4, max_length=12, required=True)
     password = forms.CharField(label="PASSWORD", min_length=6, max_length=12, required=True, widget=forms.PasswordInput)
     password_check = forms.CharField(label="PASSWORD(again)", min_length=6, max_length=12, required=True,
-                                     widget=forms.PasswordInput)
+                                     widget=forms.PasswordInput,
+                                     error_messages={'min_length': '에러! %(limit_value)d 이상 입력해주세요!',
+                                                     'max_length': '에러!'})
